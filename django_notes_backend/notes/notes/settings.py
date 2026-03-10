72e3ef94a63388402090dbb7a296e8d8063c851a
Dominik Sabota <dominik.sabota@jump-tech.eu>
2026-03-10 10:17:52 +0100

    django finished

--- django_notes_backend/notes/notes/settings.py
+++ django_notes_backend/notes/notes/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'api.apps.ApiConfig',      # ← ADDED THIS LINE
+    'rest_framework',          # ← ADDED THIS LINE
 ]
 
 MIDDLEWARE = [
